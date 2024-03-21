546ee976a4caa33b48106e3990914f231bfd8ebb
CorbinTechnology <techwork@corbintech.co.in>
2024-03-21 10:38:23 +0530

    vercel configuraiton

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   pageExtensions: ["ts", "tsx"], // Change file extensions to TypeScript
   async redirects() {
     return [
