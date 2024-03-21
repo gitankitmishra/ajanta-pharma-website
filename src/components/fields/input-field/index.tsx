114142b7d10680392f91917cb35a823861bb64c1
CorbinTechnology <techwork@corbintech.co.in>
2024-03-21 10:34:57 +0530

    feat: modules upload done

--- src/components/fields/input-field/index.tsx
+++ src/components/fields/input-field/index.tsx
@@ -7,38 +7,6 @@ interface InputFieldProps {
   readOnly?: boolean;
 }
 
-
-const InputField: FC<InputFieldProps> = ({ moduleData, index, onChange}) => {
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    const newData = [...moduleData];
-
-    if (name.includes("moduleName")) {
-      newData[index].moduleName = value;
-    } else if (name.includes("moduleNo")) {
-      newData[index].moduleNo = value;
-    }
-
-    onChange(newData);
-  };
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newData = [...moduleData]; 
-
-    newData[index].files = event.target.files;
-
-    onChange(newData);
-  };
-
-  return (
-    <div>
-      <div key={index}>
-        <input
-          className="input-field"
-          type="text"
-          name={`moduleName-${index}`}
-          value={moduleData[index]?.moduleName}
-
 const InputField: FC<InputFieldProps> = ({readOnly = false, moduleValue, onChange}) => {
     
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -50,37 +18,14 @@ const InputField: FC<InputFieldProps> = ({readOnly = false, moduleValue, onChang
           className="input-field"
           type="text"
           value={moduleValue?.length > 0 ? moduleValue[0] : ''} 
-
           onChange={handleChange}
           placeholder=""
           readOnly={readOnly}
         />
-
-        <input
-          className="input-field"
-          type="text"
-          name={`moduleNo-${index}`}
-          value={moduleData[index]?.moduleNo}
-          onChange={handleChange}
-          placeholder="Module Number"
-        />
-        <input
-          className="input-field"
-          type="file"
-          name={`files-${index}`}
-          onChange={handleFileChange}
-          placeholder="Upload File"
-        />
-      </div>
-    </div>
-  );
-};
-
   </div>;
 
   
 }
-
 
 
 export default InputField;
